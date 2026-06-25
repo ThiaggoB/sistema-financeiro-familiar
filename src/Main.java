21e7a66a7a833729c0d5ca2cc79ef94d75fe5b04
Thiaggo Bueno <Thiaggobueno25@gmail.com>
2026-06-25 18:53:08 -0300

    v1.0 - Sistema financeiro familiar

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import javax.print.DocFlavor;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[ ]args) {
+        Scanner scanner = new Scanner(System.in);
+//       ENTRADA
+
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = scanner.nextLine();
+
+       String nomecompleto = nome.concat(" ").concat(sobrenome);
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite seu salario: ");
+        double salarioMensal = scanner.nextDouble();
+
+        System.out.print("Digite o valor de suas despesas: ");
+        double despesas = scanner.nextDouble();
+
+        double saldo = salarioMensal - despesas;
+
+
+
+//        SAIDA
+        System.out.println("Nome: " + nomecompleto);
+        System.out.println("Idade : " + idade);
+        System.out.println("Salario : R$ " + salarioMensal);
+        System.out.println("Despesas: R$ " + despesas);
+
+        System.out.println("Salario restante: R$ " + saldo);
+        System.out.println("Salario Anual: R$ " + salarioMensal * 12 );
+
+        if (saldo > 1000) {
+            System.out.println("Saldo em conta dentro da normalidade. Operações seguem sem restrições ou riscos iminentes. R$ " + saldo);
+        } else if (saldo >= 200) {
+            System.out.println("Atenção: O saldo em conta está reduzindo. Recomenda-se monitorar os próximos custos ou repor fundos em breve para evitar interrupções. R$ " + saldo);
+        } else {
+            System.out.println("⚠️ Risco crítico: Saldo em conta insuficiente ou próximo ao esgotamento. Ação imediata necessária para evitar falhas nas transações. R$ " + saldo);
+        }
+        scanner.close();
+    }
+}
